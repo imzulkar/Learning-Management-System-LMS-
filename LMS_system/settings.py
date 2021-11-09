01c989779ebe0663e55e45dade1419bf5be5418e
Gm Zulkar Nine <gmzulkar@gmail.com>
2021-11-09 22:32:41 +0600

    'fixed'

--- LMS_system/settings.py
+++ LMS_system/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_DIR = BASE_DIR.joinpath('static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -152,11 +152,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = BASE_DIR.joinpath('/static/')
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [STATIC_DIR]
+
 # Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
